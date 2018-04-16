f9ac6ee6bc0fba4f974281502731fd26c4f62bf2
yunjiezhang <yunjiezhang@berkeley.edu>
2018-04-16 00:16:11 -0700

    partially finished

--- app/controllers/concerns/person.rb
+++ app/controllers/concerns/person.rb
@@ -4,21 +4,22 @@ class Person
   def initialize(name, age)
     @name = name
     @age = age
+    @nickname = name[0, 4]
   end
 
   def nickname
     # YOUR IMPLEMENTATION HERE
-    'delete_me'
+    @nickname
   end
 
   def birth_year
     # YOUR IMPLEMENTATION HERE
-    1990
+    2018 - @age.to_i
   end
 
   def introduction
     # YOUR IMPLEMENTATION HERE
-    'delete_me'
+    @name + " is " + @age
   end
 
 end
--- app/controllers/concerns/stringify.rb
+++ app/controllers/concerns/stringify.rb
@@ -8,5 +8,10 @@ class Stringify
 
   def what_am_i
   	# YOUR IMPLEMENTATION HERE
+  	if @name.blank? || @adjective.blank?
+  		"You are nothing!"
+  	else
+  		 @name.to_s + " is so " + @adjective.to_s
+  	end
   end
 end
